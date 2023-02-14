1342f61bcbda95f281c5fe5ddad5ed02f10136f7
Laure Vigouroux <laure.vigouroux@imt-atlantique.net>
2023-02-14 14:23:41 +0100

    Creation database

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
-
+from database.database import db, init_database
 app = Flask(__name__)
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///C:\\Users\\laure\\Desktop\\WEBAPP\\WebApp\\database\\database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app) # (1) flask prend en compte la base de donnee
+with app.test_request_context(): # (2) bloc exécuté à l'initialisation de Flask
+ init_database()
 
 @app.route('/')
 def hello_world():  # put application's code here
