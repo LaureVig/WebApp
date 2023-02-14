9fd959c731683c964ed8bbc5bfff4b4b147c3997
Piche23 <114235332+Piche23@users.noreply.github.com>
2023-02-14 14:05:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World! Test commit'
+    return 'Hello World ! Test commit'
 
 
 if __name__ == '__main__':
