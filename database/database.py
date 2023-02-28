0bb19990dce714b6fb7b1dff58dcace1bf183542
Laure Vigouroux <laure.vigouroux@imt-atlantique.net>
2023-02-28 09:07:54 +0100

    base de données avec un début de peuplement

--- database/database.py
+++ database/database.py
@@ -6,20 +6,69 @@ db = SQLAlchemy()
 
 
 def init_database():
+    from database.models import Promotion, Etudiant, Taf
+    db.drop_all()
+    print("drop ?")
     db.create_all()
+    print("create ?")
+    print(db.get_tables_for_bind())
     populate_database()
 
 def populate_database():
-    from database.models import Promotion, Etudiant
+    from database.models import Promotion, Etudiant, Taf,Stage, Entreprise, Personnel
+    new_taf = Taf()
+    new_taf.taf_nom='DCL'
+    new_taf.taf_description="Developpement Collaboratif de Logiciels"
+    new_taf.taf_responsable_id=1
+    new_taf.taf_domaine_id=1
+    db.session.add(new_taf)
+
     new_promo = Promotion()
-    new_promo.annee=2024
+    new_promo.pro_annee=2024
     db.session.add(new_promo)
-    db.session.commit()
+
     new_student = Etudiant()
-    new_student.nom = "Pichereau"
-    new_student.prenom = "Hugo"
-    new_student.mail = "hugo.pichereau@imt-atlantique.net"
-    new_student.promotion_id =1
+    new_student.etu_nom = "Pichereau"
+    new_student.etu_prenom = "Hugo"
+    new_student.etu_mail = "hugo.pichereau@imt-atlantique.net"
+    new_student.etu_promotion_id =1
     # Ajout de la tache dans la base de donnees
     db.session.add(new_student)
+
+    new_student2 = Etudiant()
+    new_student2.etu_nom = "Vigouroux"
+    new_student2.etu_prenom = "Laure"
+    new_student2.etu_mail = "laure.vigouroux@imt-atlantique.net"
+    new_student2.etu_promotion_id = 1
+    # Ajout de la tache dans la base de donnees
+    db.session.add(new_student2)
+
+    new_student3 = Etudiant()
+    new_student3.etu_nom = "Robidou"
+    new_student3.etu_prenom = "Guillaume"
+    new_student3.etu_mail = "guillaume.robidou@imt-atlantique.net"
+    new_student3.etu_promotion_id = 1
+    # Ajout de la tache dans la base de donnees
+    db.session.add(new_student3)
+
+    new_entreprise = Entreprise()
+    new_entreprise.ent_nom="Trecobat"
+    db.session.add(new_entreprise)
+
+    new_personnel=Personnel()
+    new_personnel.per_nom="Grijol"
+    new_personnel.per_mail="benjamin@trecobat.fr"
+    new_personnel.per_prenom="Benjamin"
+    new_personnel.per_entreprise=1
+    db.session.add(new_personnel)
+
+    new_stage=Stage()
+    new_stage.sta_etudiant= 2
+    new_stage.sta_duree=5
+    new_stage.sta_annee_etudiant=1
+    new_stage.sta_entreprise=1
+    new_stage.sta_mission="développement web"
+    new_stage.sta_tuteur=1
+    new_stage.sta_ville="Lannilis"
+    db.session.add(new_stage)
     db.session.commit()  # Sauvegarde les informations dans la base de donnees
--- database/models.py
+++ database/models.py
@@ -1,24 +1,92 @@
 from database.database import db
 
+
 class Etudiant(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nom = db.Column(db.Text, nullable=False)
-    prenom = db.Column(db.Text, nullable=False)
-    mail = db.Column(db.Text, nullable=False)
-    promotion_id = db.Column(db.Integer, db.ForeignKey('promotion.id'))
+    etu_id = db.Column(db.Integer, primary_key=True)
+    etu_nom = db.Column(db.Text, nullable=False)
+    etu_prenom = db.Column(db.Text, nullable=False)
+    etu_mail = db.Column(db.Text, nullable=False)
+    etu_promotion_id = db.Column(db.Integer, db.ForeignKey('promotion.pro_id'))
+    etu_lien_taf = db.relationship('LienEtudiantTaf', backref='etudiant')
 
     def __repr__(self):
         return '<Etudiant {}>'.format(self.nom)
+
     """taf1 = 
     taf2 = 
     stageA1
     stageA2
     stageA3"""
 
+
 class Promotion(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    annee = db.Column(db.Integer, nullable = False)
-    promotion = db.relationship('Etudiant', backref='promotion')
+    pro_id = db.Column(db.Integer, primary_key=True)
+    pro_annee = db.Column(db.Integer, nullable=False)
+    pro_promotion = db.relationship('Etudiant', backref='promotion')
 
     def __repr__(self):
         return '<Promotion {}>'.format(self.annee)
+
+
+class Taf(db.Model):
+    taf_id = db.Column(db.Integer, primary_key=True)
+    taf_nom = db.Column(db.Text, nullable=False)
+    taf_description = db.Column(db.Text, nullable=False)
+    taf_responsable_id = db.Column(db.Integer, db.ForeignKey('enseignant.ens_id'), nullable=False)
+    taf_domaine_id = db.Column(db.Integer, db.ForeignKey('domaine.dom_id'), nullable=False)
+    taf_lien_etudiant = db.relationship('LienEtudiantTaf', backref='taf')
+    taf_lien_ue = db.relationship('LienUeTaf', backref='taf')
+
+    def __repr__(self):
+        return '<TAF {}>'.format(self.nom)
+
+
+class LienEtudiantTaf(db.Model):
+    let_etudiant_id = db.Column(db.Integer, db.ForeignKey('etudiant.etu_id'), primary_key=True)
+    let_taf_id = db.Column(db.Integer, db.ForeignKey('taf.taf_id'), primary_key=True)
+
+
+class Enseignant(db.Model):
+    ens_id = db.Column(db.Integer, primary_key=True)
+    ens_nom = db.Column(db.Text, nullable=False)
+    ens_prenom = db.Column(db.Text, nullable=False)
+    ens_mail = db.Column(db.Text, nullable=False)
+
+
+class Domaine(db.Model):
+    dom_id = db.Column(db.Integer, primary_key=True)
+    dom_nom = db.Column(db.Text, nullable=False)
+
+
+class Stage(db.Model):
+    sta_id = db.Column(db.Integer, primary_key=True)
+    sta_etudiant = db.Column(db.Integer, db.ForeignKey('etudiant.etu_id'))
+    sta_entreprise = db.Column(db.Integer, db.ForeignKey('entreprise.ent_id'))
+    sta_tuteur = db.Column(db.Integer, db.ForeignKey('personnel.per_id'))
+    sta_mission = db.Column(db.Text)
+    sta_duree = db.Column(db.Integer)
+    sta_annee_etudiant = db.Column(db.Integer)
+    sta_ville = db.Column(db.Text)
+
+
+class Ue(db.Model):
+    ue_id = db.Column(db.Integer, primary_key=True)
+    ue_nom = db.Column(db.Text)
+    ue_description = db.Column(db.Text)
+    ue_responsable_id = db.Column(db.Integer, db.ForeignKey('enseignant.ens_id'), nullable=False)
+
+
+class LienUeTaf(db.Model):
+    lut_ue_id = db.Column(db.Integer, db.ForeignKey('ue.ue_id'), primary_key=True)
+    lut_taf_id = db.Column(db.Integer, db.ForeignKey('taf.taf_id'), primary_key=True)
+
+class Entreprise(db.Model):
+    ent_id =db.Column(db.Integer, primary_key=True)
+    ent_nom = db.Column(db.Text)
+
+class Personnel(db.Model):
+    per_id = db.Column(db.Integer, primary_key=True)
+    per_nom =db.Column(db.Text)
+    per_prenom =db.Column(db.Text)
+    per_mail =db.Column(db.Text)
+    per_entreprise = db.Column(db.Integer, db.ForeignKey('entreprise.ent_id'))
